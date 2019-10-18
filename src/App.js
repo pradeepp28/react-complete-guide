840c45c880239a2850fdf8bae5ede0273339e196
Pradeep Palanisamy <ppalanisamy_cht@magicleap.com>
2019-10-18 08:05:03 -0400

    toggle persons using java script

--- src/App.js
+++ src/App.js
@@ -45,15 +45,12 @@ class App extends Component {
       padding: "8px",
       cursor: "pointer"
     }
-    return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <p>This is working</p>
-        <button 
-          style={style}
-          onClick = {this.togglePersonsHandker}>Toggle Persons</button>
-        { this.state.showPersons ?
-          <div>
+
+    let persons = null
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
             <Person 
               name = {this.state.persons[0].name} 
               age = {this.state.persons[0].age}/>
@@ -65,8 +62,17 @@ class App extends Component {
             <Person 
               name = {this.state.persons[2].name} 
               age = {this.state.persons[2].age}/>
-          </div> : null
-        }
+          </div>
+      )
+    }
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p>This is working</p>
+        <button 
+          style={style}
+          onClick = {this.togglePersonsHandker}>Toggle Persons</button>
+        {persons}
       </div>
     );
     //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App'))
