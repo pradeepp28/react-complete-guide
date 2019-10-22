7a2f3839313f8be98d7fa51a65bbe5768413575b
Pradeep Palanisamy <ppalanisamy_cht@magicleap.com>
2019-10-22 08:21:00 -0400

    dynamic style and radium

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Radium, { StyleRoot } from  'radium'
 import Person from './Person/Person'
 
 class App extends Component {
@@ -53,11 +54,16 @@ class App extends Component {
 
   render() {
     const style = {
-      backgroundColor: "white",
+      backgroundColor: "green",
+      color: "white",
       font: "inherit",
       border: "1px solid blue",
       padding: "8px",
-      cursor: "pointer"
+      cursor: "pointer",
+      ":hover": {
+        backgroundColor: "lightGreen",
+        color: "black"
+      }
     }
 
     let persons = null
@@ -73,20 +79,36 @@ class App extends Component {
           changed = {(event) => this.nameChangeHandler(event, person.id)}/>
         })
       )
+      style.backgroundColor = "red"
+      style[':hover'] = {
+        backgroundColor: "salmon",
+        color: "black"
+      }
     }
 
+    const classes = []
+
+    if (this.state.persons.length <= 2) {
+      classes.push("red")
+    }
+
+    if (this.state.persons.length <= 1) {
+      classes.push("bold")
+    }
     return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <p>This is working</p>
-        <button 
-          style={style}
-          onClick = {this.togglePersonsHandker}>Toggle Persons</button>
-        {persons}
-      </div>
+      <StyleRoot>
+        <div className="App">
+          <h1>Hi, I'm a React App</h1>
+          <p className={classes.join(' ')}>This is working</p>
+          <button 
+            style={style}
+            onClick = {this.togglePersonsHandker}>Toggle Persons</button>
+          {persons}
+        </div>
+      </StyleRoot>
     );
     //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App'))
   }
 }
 
-export default App;
+export default Radium(App);
