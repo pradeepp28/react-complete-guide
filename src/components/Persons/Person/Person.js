d2fca8d2bfbd1c1d1542e29bd15177d32cb82703
Pradeep Palanisamy <ppalanisamy_cht@magicleap.com>
2019-11-24 07:19:51 -0500

    Ref class based component

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -6,6 +6,16 @@ import Aux from "../../../hoc/Aux";
 import withClass from "../../../hoc/WithClass";
 
 class Person extends Component {
+  constructor(props) {
+    super(props);
+    this.inputElementRef = React.createRef();
+  }
+
+  componentDidMount() {
+    //this.inputElement.focus();
+    this.inputElementRef.current.focus();
+  }
+
   render() {
     console.log("[Person.js] rendering ...");
     return (
@@ -16,6 +26,8 @@ class Person extends Component {
         <p>{this.props.children}</p>
         <input
           type="text"
+          //ref={inputEl => (this.inputElement = inputEl)}
+          ref={this.inputElementRef}
           onChange={this.props.changed}
           value={this.props.name}
         />
@@ -30,4 +42,5 @@ Person.propTypes = {
   age: PropTypes.number,
   changed: PropTypes.func
 };
+
 export default withClass(Person, classes.Person);
